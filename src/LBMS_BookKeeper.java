e512fad900e9179aa3ab435019e46cb7f85e8489
pdfkpb <pdfkkpb@gmail.com>
2017-03-04 16:23:04 -0500

    Added some stoof

--- src/LBMS_BookKeeper.java
+++ src/LBMS_BookKeeper.java
@@ -4,4 +4,18 @@
 
 public class LBMS_BookKeeper
 {
+    public Visitor registerVisitor(String firstName, String lastName, String address, String phoneNumber)
+    {
+
+    }
+
+    private void startup()
+    {
+
+    }
+
+    private void shutdown()
+    {
+
+    }
 }
