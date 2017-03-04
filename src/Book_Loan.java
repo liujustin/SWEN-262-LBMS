688e94fb15b6d3c96e8c5fc501073920fb787668
Ryan Connors <rjc2872@rit.edu>
2017-03-04 16:13:49 -0500

    Implemented Visitor and started Book_loan

--- src/Book_Loan.java
+++ src/Book_Loan.java
@@ -4,4 +4,15 @@
 
 public class Book_Loan
 {
+    private Visitor loaned_to;
+    private Book book;
+    private double balance;
+    private boolean active_balance;
+
+    public Book_Loan(Visitor loaned_to, Book book, double balance, boolean active_balance) {
+        this.loaned_to = loaned_to;
+        this.book = book;
+        this.balance = balance;
+        this.active_balance = active_balance;
+    }
 }
--- src/Visitor.java
+++ src/Visitor.java
@@ -4,4 +4,22 @@
 
 public class Visitor
 {
+    private String first_name;
+    private String last_name;
+    private String address;
+    private double balance;
+    private String phone_number;
+    private int visitor_ID;
+
+    public Visitor(String first_name, String last_name, String address,
+                   double balance, String phone_number, int visitor_ID) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.balance = balance;
+        this.phone_number = phone_number;
+        this.visitor_ID = visitor_ID;
+    }
+
+
 }
