c2ddb091bf5ca91171c20ba9476eb3fc938173fe
Ryan Connors <rjc2872@rit.edu>
2017-03-04 16:22:15 -0500

    Added getters/setters to visitor and book_loan

--- src/Book_Loan.java
+++ src/Book_Loan.java
@@ -15,4 +15,36 @@ public class Book_Loan
         this.balance = balance;
         this.active_balance = active_balance;
     }
+
+    public Visitor getLoaned_to() {
+        return loaned_to;
+    }
+
+    public void setLoaned_to(Visitor loaned_to) {
+        this.loaned_to = loaned_to;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean isActive_balance() {
+        return active_balance;
+    }
+
+    public void setActive_balance(boolean active_balance) {
+        this.active_balance = active_balance;
+    }
 }
--- src/Visitor.java
+++ src/Visitor.java
@@ -21,5 +21,51 @@ public class Visitor
         this.visitor_ID = visitor_ID;
     }
 
+    public String getFirst_name() {
+        return first_name;
+    }
 
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public int getVisitor_ID() {
+        return visitor_ID;
+    }
+
+    public void setVisitor_ID(int visitor_ID) {
+        this.visitor_ID = visitor_ID;
+    }
 }
