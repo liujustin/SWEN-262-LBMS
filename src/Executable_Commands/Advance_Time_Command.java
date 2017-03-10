9dccfc23cd23a72eb998a7e909a59e9d2f8b4da4
akn4743 <akn4743@rit.edu>
2017-03-10 10:28:11 -0500

    Executable commands folder pushed

---
+++ src/Executable_Commands/Advance_Time_Command.java
@@ -0,0 +1,11 @@
+package Executable_Commands;
+
+/**
+ * Created by adamn on 3/2/2017.
+ */
+public class Advance_Time_Command implements Command {
+    @Override
+    public void execute() {
+
+    }
+}
