0aa0849eed16ff58a49f33e01a0c7903abc6cc05
pdfkpb <pdfkkpb@gmail.com>
2017-03-20 00:02:33 -0400

    Added more function to Bookkeeper

--- src/LBMS_BookKeeper.java
+++ src/LBMS_BookKeeper.java
@@ -36,8 +36,7 @@ public class LBMS_BookKeeper
                 Book tempBook = new Book(tempLine[0],
                                          tempLine[1],
                                          tempLine[3],
-                                         tempLine[4],
-                                        false);
+                                         tempLine[4]);
 
                 ArrayList<String> authors = new ArrayList<>();
                 String[] tempAuthors = tempLine[2].split(",");
@@ -66,9 +65,18 @@ public class LBMS_BookKeeper
         }
     }
 
-    public Book buyBook()
+    public void buyBook(Integer quantity, ArrayList<String> ISBNS)
     {
-
+        for(String isbn : ISBNS)
+        {
+            for(int i = 0; i < quantity; ++ i)
+            {
+                if(this.purchasedBooks.containsKey(this.bookRegistry.get(isbn)))
+                    this.purchasedBooks.put(this.bookRegistry.get(isbn), this.purchasedBooks.get(this.bookRegistry.get(isbn)) + 1);
+                else
+                    this.purchasedBooks.put(this.bookRegistry.get(isbn), 1);
+            }
+        }
     }
 
     public void borrowBook(Visitor visitor, String bookList) throws Exception
@@ -78,11 +86,28 @@ public class LBMS_BookKeeper
         if(bookISBN.length > 5 - visitor.getBorrowed_books().size())
             throw new Exception("borrow,book-limit-exceeded;");
 
+        if(visitor.getBalance() > 0)
+            throw new Exception("borrow,outstanding-fine,"+visitor.getBalance()+';');
+
+        ArrayList<String> invalidBookIDs = new ArrayList<>();
         for(int i=0; i<bookISBN.length; ++i)
         {
             if(!this.purchasedBooks.containsKey(bookISBN[i]))
-                throw new Exception("");
+                invalidBookIDs.add(bookISBN[i]);
         }
+
+        if(invalidBookIDs.size() > 0)
+        {
+            String errorString = "borrow,invalid-book-id,{";
+            for(String isbn: invalidBookIDs)
+                errorString += isbn+", ";
+            errorString = errorString.subSequence(0, errorString.length()-1).toString()+"}";
+
+            throw new Exception(errorString);
+        }
+
+        for(String isbn: bookISBN)
+            visitor.add_book(new Book_Loan(visitor, this.bookRegistry.get(isbn), 0.0, true));
     }
 
     public static void main(String[] args)
