ca70d484ee13234342daf3e55a5273cd5fc2b66b
akn4743 <akn4743@rit.edu>
2017-03-29 21:47:35 -0400

    + fixed bug where advance time wasnt working + fixed register, begin visit, end visit commands to output correct date + added bugs & ToDo list to help keep track of what needs to be done and fixed

--- src/LBMS_StatisticsKeeper.java
+++ src/LBMS_StatisticsKeeper.java
@@ -1,33 +1,54 @@
 //FILE::LBMS_StatisticsKeeper.java
-//AUTHOR::Justin Liu
+//AUTHOR::Justin Liu, Adam Nowak
 //DATE::Mar.04.2017
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LBMS_StatisticsKeeper
 {
 	private static Date date;
 	private Calendar calendar;
+	private static int seconds = 0;
+	static Timer timer = new Timer();
+	static TimerTask task = new TimerTask() {
+		@Override
+		public void run() {
+			seconds++;
+		}
+	};
 
 	public LBMS_StatisticsKeeper(){
 		this.calendar = Calendar.getInstance();
 		this.date = new Date();
+
+
+
 	}
 
 	/**
 	 * gets the datetime for the datetime command
 	 */
-
 	public static String Get_Time() {
-		DateFormat dateFormat = new SimpleDateFormat ("MM/dd/yyyy HH:mm:ss");
+		if(seconds == 0) {
+			timer.scheduleAtFixedRate(task, 1000, 1000);
+		}
+		DateFormat dateFormat = new SimpleDateFormat ("MM/dd/yyyy,HH:mm:ss");
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		c.add(Calendar.SECOND, seconds);
+		date.setTime(c.getTimeInMillis());
 		String output = dateFormat.format(date);
-		System.out.println(output);
 		return output;
 	}
-
+	public void printTime(){
+		String d2 = LBMS_StatisticsKeeper.Get_Time();
+		System.out.println("datetime," + d2);
+	}
 	public void advanceDay(int days){
 		Calendar c = Calendar.getInstance();
 		c.setTime(date);
