d27945d46e3a3f84925ea9c13d781f4c124e45d5
Ryan Connors <rjc2872@rit.edu>
2017-04-08 13:57:43 -0400

    Added account class with create account function.

---
+++ src/Account.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Ryan on 4/7/2017.
+ */
+public class Account {
+
+    private String username;
+    private String password;
+    private int role;
+    private long visitorID;
+    private Visitor v;
+
+    public Account(String username, String password, int role, long visitorID) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.visitorID = visitorID;
+        this.v = Main.vk.getVisitorRegistry().get(this.visitorID);
+    }
+
+    public Account createAccount(String username, String password, int role, long visitorID){
+        Account a = new Account(username,password,role,visitorID);
+        System.out.println("create,success");
+        return a;
+    }
+}
